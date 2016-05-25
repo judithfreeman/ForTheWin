5bb4de020e53ec302000cdc4a459c137ed344b30
Judith Freeman <judithfreeman@gmail.com>
2016-05-25 13:49:58 -0700

    Added twitter photo

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -141,23 +141,18 @@ function slidePageFrom(page, from) {
 }
 
     
-function openCity(evt, cityName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
+jQuery(document).ready(function() {
+	jQuery('.tabs .tab-links a').on('click', function(e)  {
+		var currentAttrValue = jQuery(this).attr('href');
+ 
+		// Show/Hide Tabs
+		jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
+ 
+		// Change/remove current tab to active
+		jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+ 
+		e.preventDefault();
+	    });
+    });
 
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tabcontent.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    // Show the current tab, and add an "active" class to the link that opened the tab
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
